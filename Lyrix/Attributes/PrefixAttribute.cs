1d7343642014de85ed35bc8aa57ccf917dfabac8
shirbr510 <shirbr510@gmail.com>
2015-08-26 00:19:23 +0300

    Added PrefixAttribute (Got missing in previous Commit)

---
+++ Lyrix/Attributes/PrefixAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lyrix.Attributes
+{
+    /// <summary>
+    /// Specifies a prefix for a TagsObject.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    class PrefixAttribute : Attribute
+    {
+        public PrefixAttribute(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; set; }
+    }
+}
